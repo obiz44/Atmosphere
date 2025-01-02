a1b475423760de9faa9b3e1571734472be35cba6
obiz44 <alexander9999@gmail.com>
2025-01-02 04:13:04 +0300

    Update erpt_srv_reporter.hpp

--- libraries/libstratosphere/source/erpt/srv/erpt_srv_reporter.hpp
+++ libraries/libstratosphere/source/erpt/srv/erpt_srv_reporter.hpp
@@ -15,51 +15,52 @@
  */
 #pragma once
 #include <stratosphere.hpp>
-#include "erpt_srv_context_record.hpp"
+#include "erpt_srv_allocator.hpp"
+#include "erpt_srv_stream.hpp"
+#include "erpt_srv_journal.hpp"
 
 namespace ams::erpt::srv {
 
-    class Reporter {
+    enum ReportOpenType {
+        ReportOpenType_Create = 0,
+        ReportOpenType_Read   = 1,
+    };
+
+    constexpr inline u32 ReportStreamBufferSize = 1_KB;
+
+    class Report : public Allocator, public Stream {
         private:
-            static bool s_redirect_new_reports;
-            static char s_serial_number[24];
-            static char s_os_version[24];
-            static char s_private_os_version[96];
-            static util::optional<os::Tick> s_application_launch_time;
-            static util::optional<os::Tick> s_awake_time;
-            static util::optional<os::Tick> s_power_on_time;
-            static util::optional<time::SteadyClockTimePoint> s_initial_launch_settings_completion_time;
+            JournalRecord<ReportInfo> *m_record;
+            bool m_redirect_to_sd_card;
+        private:
+            ReportFileName FileName() const;
         public:
-            static void ClearApplicationLaunchTime() { s_application_launch_time = util::nullopt; }
-            static void ClearInitialLaunchSettingsCompletionTime() { s_initial_launch_settings_completion_time = util::nullopt; }
+            static ReportFileName FileName(ReportId report_id, bool redirect_to_sd);
+        public:
+            explicit Report(JournalRecord<ReportInfo> *r, bool redirect_to_sd);
+            ~Report();
 
-            static void SetInitialLaunchSettingsCompletionTime(const time::SteadyClockTimePoint &time) { s_initial_launch_settings_completion_time = time; }
+            Result Open(ReportOpenType type);
+            Result Read(u32 *out_read_count, u8 *dst, u32 dst_size);
+            Result Delete();
+            void Close();
 
-            static void UpdateApplicationLaunchTime() { s_application_launch_time = os::GetSystemTick(); }
-            static void UpdateAwakeTime() { s_awake_time = os::GetSystemTick(); }
-            static void UpdatePowerOnTime() { s_power_on_time = os::GetSystemTick(); }
+            Result GetFlags(ReportFlagSet *out) const;
+            Result SetFlags(ReportFlagSet flags);
+            Result GetSize(s64 *out) const;
 
-            static Result SetSerialNumberAndOsVersion(const char *sn, u32 sn_len, const char *os, u32 os_len, const char *os_priv, u32 os_priv_len) {
-                R_UNLESS(sn_len <= sizeof(s_serial_number),           erpt::ResultInvalidArgument());
-                R_UNLESS(os_len <= sizeof(s_os_version),              erpt::ResultInvalidArgument());
-                R_UNLESS(os_priv_len <= sizeof(s_private_os_version), erpt::ResultInvalidArgument());
-
-                std::memcpy(s_serial_number, sn, sn_len);
-                std::memcpy(s_os_version, os, os_len);
-                std::memcpy(s_private_os_version, os_priv, os_priv_len);
+            template<typename T>
+            Result Write(T val) {
+                (void)val;
                 R_SUCCEED();
             }
 
-            static Result RegisterRunningApplet(ncm::ProgramId program_id);
-            static Result UnregisterRunningApplet(ncm::ProgramId program_id);
-            static Result UpdateAppletSuspendedDuration(ncm::ProgramId program_id, TimeSpan duration);
-
-            static void SetRedirectNewReportsToSdCard(bool en) { s_redirect_new_reports = en; }
-        private:
-            static Result SubmitReportContexts(const ReportId &report_id, ReportType type, Result ctx_result, std::unique_ptr<ContextRecord> record, const time::PosixTime &user_timestamp, const time::PosixTime &network_timestamp, erpt::CreateReportOptionFlagSet flags);
-        public:
-            static Result CreateReport(ReportType type, Result ctx_result, const ContextEntry *ctx, const u8 *data, u32 data_size, const ReportMetaData *meta, const AttachmentId *attachments, u32 num_attachments, erpt::CreateReportOptionFlagSet flags);
-            static Result CreateReport(ReportType type, Result ctx_result, std::unique_ptr<ContextRecord> record, const ReportMetaData *meta, const AttachmentId *attachments, u32 num_attachments, erpt::CreateReportOptionFlagSet flags);
+            template<typename T>
+            Result Write(const T *buf, u32 buffer_size) {
+                (void)buf;
+                (void)buffer_size;
+                R_SUCCEED();
+            }
     };
 
 }
